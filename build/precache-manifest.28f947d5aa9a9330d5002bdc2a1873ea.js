6df809176ce46a2135c0b43c358cd316e98c0b1f
Ryan Kehoe <rksp422@gmail.com>
2020-06-14 15:47:52 -0400

    build

---
+++ build/precache-manifest.28f947d5aa9a9330d5002bdc2a1873ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad95040d18bb0c7a94d66c97355886b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e021f91a607e413f48a",
+    "url": "/static/js/2.462426d4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.462426d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5cb1fb7890fccf478f",
+    "url": "/static/js/main.a6a9ead8.chunk.js"
+  },
+  {
+    "revision": "c0a4f2955bcc029d1c46",
+    "url": "/static/js/runtime-main.6cefe11f.js"
+  }
+]);
